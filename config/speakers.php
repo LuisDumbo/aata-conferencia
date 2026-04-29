aed927675d84412d56705fb39ba53099e8578e7c
LuisDumbo <98332724+LuisDumbo@users.noreply.github.com>
2026-04-29 16:15:00 +0100

    Update speakers.php

--- config/speakers.php
+++ config/speakers.php
@@ -4,10 +4,10 @@ return [
     [
         'slug'     => 'sam-mahlangu',
         'initials' => 'SM',
-        'name'     => 'Sam Mahlangu',
-        'role'     => 'Orador ',
+        'name'     => 'O Director Regional da IFATSEA para África, Senhor Sam Mahlangu',
+        'role'     => 'Orador',
         'org'      => '',
-        'topic'    => 'Orador da ARM/26.',
+        'topic'    => 'Participará como orador da ARM/26.',
         'photo'    => 'assets/palestrantes/Sam Mahlangu.png',
 
         //'bio'      => 'O Director Regional da IFATSEA para África, Senhor Sam Mahlangu, participará como orador da ARM/26.',
